4b4c4253eaf306a9bd1b8c08c3d89b38f16bd0bb
Roivincent065 <salimbagatroivincent@gmail.com>
2026-08-16 07:21:12 +0800

    remove API_URL variable and import api.js to use apiFetch instead

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const API_URL = "https://task-api-backend-zgqe.onrender.com";
+import { apiFetch } from "./api.js";
 
 const loginForm = document.querySelector("#login-form");
 const message = document.querySelector("#message");
@@ -9,22 +9,28 @@ loginForm.addEventListener("submit", async (event) => {
     const email = document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
 
-    const response = await fetch(`${API_URL}/auth/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-    });
+    try {
+        const response = await apiFetch("/auth/login", {
+            method: "POST",
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    if (!response.ok) {
-        message.textContent = data.message;
-        return;
+        if (!response.ok) {
+            message.textContent = data.message;
+            return;
+        }
+
+        localStorage.setItem("accessToken", data.accessToken);
+
+        window.location.href = "tasks.html";
+
+    } catch (error) {
+        console.error(error);
+        message.textContent = "Unable to connect to server";
     }
-
-    localStorage.setItem("accessToken", data.accessToken);
-
-    window.location.href = "tasks.html";
 });
